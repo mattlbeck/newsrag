15eb3aa619cb3cfb346db8a6a765c35b671e10cb
mattlbeck <17108752+mattlbeck@users.noreply.github.com>
2024-11-19 13:40:45 +0000

    refactored streaming and citation management

--- app.py
+++ app.py
@@ -1,71 +1,24 @@
 import gradio as gr
+from generator import Sources, StreamingText
 from rag import RAG, RAGSummariser
-from feeds import TheGuardian, AssociatedPress, BBC, download_feeds
-from haystack_integrations.components.generators.ollama import OllamaGenerator
+import feeds
+import generator
+
 
 from multiprocessing.pool import ThreadPool
-from queue import Queue
-from top2vec import Top2Vec
-from haystack import Document
-
-class StreamingText:
-
-    def __init__(self):
-        self._text = Queue()
-        self._done = False
-
-    def __call__(self, text_chunk):
-        if text_chunk.meta["done"]:
-            self._done = True
-        self._text.put(text_chunk.content)
-
-    def __iter__(self):
-        return self
-    
-    def __next__(self):
-        if self._done:
-            raise StopIteration()
-        return self._text.get()
-
-
-
-news = download_feeds((TheGuardian, AssociatedPress, BBC))
-
-def describe_topic(topic_words):
-    llm = OllamaGenerator(model="llama3.2")
-    prompt = f"""
-    Below is a list of keywords that all derive from one topic. Please provide a short description, maximum 5 words, of the topic that best fits. Output only the topic description.
-    Keywords: {topic_words[:10]}
-    Topic: 
-    """
-    return llm.run(prompt=prompt)["replies"][0]
+from topics import DocumentTopics
+import re
 
 
 with gr.Blocks() as demo:
-    topic_selection = gr.Dropdown(label="Select a topic", type="index")
-    topic_model = gr.State()
-
-    chatbot = gr.Chatbot(type="messages")
-   
-    query = gr.Textbox()
-    topic_model = gr.State()
 
     def topics():
-        Top2Vec.contextual_top2vec = False
-        model = Top2Vec(
-            [d.content for d in news], 
-            embedding_model='universal-sentence-encoder', 
-            min_count=1, 
-            speed="deep-learn", 
-            ngram_vocab=False,
-            keep_documents=True
-        )
-        num_topics = model.get_num_topics()
-        topic_words, word_scores, topic_nums = model.get_topics(num_topics)
-        topic_descriptions = []
-        for topic in topic_words:
-            topic_descriptions.append(describe_topic(topic))
-        return model, gr.update(choices=topic_descriptions, value=None)
+        news = feeds.download_feeds((feeds.TheGuardian, feeds.AssociatedPress, feeds.BBC, feeds.ABC, feeds.CNBC, feeds.FoxNews, feeds.EuroNews ))
+        print(f"{len(news)} news articles")
+        model = DocumentTopics(news)
+        model.run()
+        
+        return model, gr.update(choices=model.get_topic_descriptions(), value=None)
 
 
     def user(user_message, history:list):
@@ -74,23 +27,27 @@ with gr.Blocks() as demo:
             history = []
         return "", history + [{"role": "user", "content": user_message}]
     
-    def summarise(topic_model, topic_num: int):
+    def summarise(topic_model: DocumentTopics, topic_num: int):
         """This clears the chat history and starts again with a new news summary"""
-        documents, document_scores, document_ids = topic_model.search_documents_by_topic(topic_num=topic_num, num_docs=10)
+        
         newsrag = RAGSummariser()
-        streamer = StreamingText()
-        newsrag.llm.streaming_callback = streamer
 
-        pool = ThreadPool(processes=1)
-        async_result = pool.apply_async(newsrag.run, kwds={"documents": [Document(content=d) for d in documents]}, error_callback=lambda x: print("Error in generation thread: ", x))
+        topic_documents = topic_model.get_documents_for_topic(topic_num)
+
+        async_result = newsrag.run_async(topic_documents)
 
         history = [{"role": "assistant", "content": ""}]
-        for new_token in iter(streamer):
-            history[0]["content"] += new_token
-            yield history
-        
+        # Run the news summarisation pipeline
+        for content, sources in newsrag.stream_output(topic_documents):
+            history[0]["content"] = content
+            bibliography = "\n".join(sources.generate_bibliography())
+            yield history, bibliography
+
         pipeline_result = async_result.get()
-        yield [{"role": "assistant", "content": pipeline_result["llm"]["replies"][0]}]
+        final_output = [{"role": "assistant", "content": pipeline_result["llm"]["replies"][0]}]
+        history.append({"role": "user", "content": pipeline_result["prompt_builder"]["prompt"]})
+        history.append({"role": "assistant", "content": pipeline_result["llm"]["replies"][0]})
+        yield history,  "\n".join(sources.generate_bibliography())
 
     def bot(history: list):
         newsrag = RAG(documents=news)
@@ -108,10 +65,20 @@ with gr.Blocks() as demo:
             yield history, ""
         
         pipeline_result = async_result.get()
-        history[-1] = {"role": "assistant", "content": pipeline_result["llm"]["replies"][0]}
+        
         yield history, pipeline_result["prompt_builder"]["prompt"]
 
     #text.submit(summarise, [text], [chatbot])
+    with gr.Row():
+        title = gr.Markdown("# newsrag")
+    with gr.Row():
+        with gr.Column():
+            topic_selection = gr.Dropdown(label="Select a topic", type="index")
+            topic_model = gr.State()
+            chatbot = gr.Chatbot(type="messages", height=600)
+        with gr.Column():
+            sources = gr.Markdown("Sources go here", container=True, height=800)
+    topic_model = gr.State()
     demo.load(topics, outputs=[topic_model, topic_selection])
-    topic_selection.select(summarise, [topic_model, topic_selection], [chatbot])
+    topic_selection.select(summarise, [topic_model, topic_selection], [chatbot, sources])
 demo.launch()
--- rag.py
+++ rag.py
@@ -9,6 +9,9 @@ from haystack.components.builders.answer_builder import AnswerBuilder
 from haystack.components.builders.prompt_builder import PromptBuilder
 from functools import cached_property
 import arrow
+from multiprocessing.pool import ThreadPool
+
+import generator
 
 
 def get_embedding_retriever(docs, top_k):
@@ -59,6 +62,7 @@ class RAG:
     
     def run(self, question):
         # Ask a question
+        min_date = arrow.utcnow().shift(days=-1)
         results = self.pipeline.run(
             {
                 "embedder": {"text": question},
@@ -66,23 +70,22 @@ class RAG:
                 "retriever": {"filters": {"field": "meta.date", "operator": ">", "value": min_date }}
             }, include_outputs_from=("retriever", "prompt_builder", "llm")
         )
-        print(results)
         return results
     
 
 class RAGSummariser(RAG):
 
-    prompt_template = """
-        You will be provided with a list of news articles from today. Write a few paragraphs that summarises selected events. Do not refer to the existance of the news articles themselves, their titles, or their formatting. With each statement you provide, leave 1 or more citations in the form of [ARTICLE NUMBER], where ARTICLE NUMBER corresponds to the number of the articles from which you sourced that statement. You may source a statement from more than one article and site them as [ARTICLE 1, ARTICLE 2, etc].
+    prompt_template = """You will be provided with a list of news articles from today. Write a few paragraphs that summarises the news. Do not refer to the existance of the news articles themselves, their titles, or their formatting. After each statement, provide one or more citations in the form "[ARTICLE 1]", where ARTICLE 1 corresponds to the identifier of the article from which you sourced your statement. You may source a statement from more than one article, for example "[ARTICLE 1, ARTICLE 2]". Place these citations within the sentence e.g. "This is a statement [ARTICLE 1]."
 
-        News articles:
-        {% for doc in documents %}
-            ARTICLE {{ loop.index }}: {{ doc.content }}
-        {% endfor %}
-        """
+News articles:
+{% for doc in documents %}
+ARTICLE {{ loop.index }}: {{ doc.content }}
+{% endfor %}
+
+Summary:"""
     def __init__(self):
         self.prompt_builder = PromptBuilder(template=self.prompt_template)
-        self.llm = OllamaGenerator(model="llama3.2")
+        self.llm = OllamaGenerator(model="llama3.2", generation_kwargs={"num_ctx": 4096})
 
         self.pipeline = Pipeline()
         
@@ -101,6 +104,24 @@ class RAGSummariser(RAG):
             include_outputs_from=("prompt_builder", "llm")
         )
         return results
+    
+    def run_async(self, documents):
+        streamer = generator.StreamingText()
+        self.llm.streaming_callback = streamer
+
+        pool = ThreadPool(processes=1)
+  
+        async_result = pool.apply_async(self.run, kwds={"documents": documents}, error_callback=lambda x: print("Error in generation thread: ", x))
+        return async_result
+
+    
+    def stream_output(self, documents: list[Document]):
+    
+        # Stream the summary output, transforming citations
+        # on the fly and building a bibliography to output to a second
+        # component
+        yield from generator.stream_sourced_output(iter(self.llm.streaming_callback), documents)
+
 
 
 if __name__ == "__main__":
