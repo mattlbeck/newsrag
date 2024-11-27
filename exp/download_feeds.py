7ae5162b4f817fb172a013598b3658509cb8fc6d
mattlbeck <17108752+mattlbeck@users.noreply.github.com>
2024-11-27 23:21:00 +0000

    exp: added new feeds and re-ran pipeline

--- exp/download_feeds.py
+++ exp/download_feeds.py
@@ -2,12 +2,19 @@ import newsrag.feeds as feeds
 from pathlib import Path
 import json
 import jsonlines
+from collections import defaultdict
 
 if __name__ == "__main__":
     data_dir = Path("./data")
     data_dir.mkdir(exist_ok=True)
 
     docs = feeds.download_feeds()
+    vendor_count = defaultdict(int)
+    for doc in docs:
+        for subfeed in doc.meta["subfeeds"]:
+            vendor_count[doc.meta["vendor"] + "." + subfeed] += 1
+    print(vendor_count)
+    
     # serialize to json-lines
     out_file =  data_dir / "documents.jsonl"
     with jsonlines.Writer(out_file.open("w")) as writer:
